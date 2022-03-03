afb87377593ff59adb79237d054bcea42c5b485f
Tom Poyarkov <poart1405@gmail.com>
2022-03-04 00:46:41 +0300

    update user domain model

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -2,6 +2,7 @@ package domain
 
 type User struct {
 	Id          int32  `json:"id"`
+	Verified    bool   `json:"verified"`
 	Name        string `json:"name"`
 	Surname     string `json:"surname"`
 	CompanyName string `json:"companyName"`
